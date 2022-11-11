d363d0726b01de4aa57f1e4a721b6ca6edecd029
Alaa Aldandachi <58466561+AlaaDan@users.noreply.github.com>
2022-11-11 15:19:25 +0100

    Co-authored-by: Hamdi Al Masalmeh <P156HAM@users.noreply.github.com>

---
+++ Word Puzzle/puzzle.js
@@ -0,0 +1,123 @@
+const wordsWithMsg = [
+    {
+        msg: "Raise FOUR to FIVE",
+        startWord: "FOUR",
+        endWord: "FIVE"
+    },
+    {
+        msg: "Cover EYE with LID",
+        startWord: "EYE",
+        endWord: "LID"
+    },
+    {
+        msg: "Crown TIGER with ROSES",
+        startWord: "TIGER",
+        endWord: "ROSES"
+    },
+    {
+        msg: "Make WHEAT into BREAD",
+        startWord: "WHEAT",
+        endWord: "BREAD"
+    }
+
+];
+
+
+
+async function gameEngin (object, newWord) {
+    const BASE_URL = 'https://api.dictionaryapi.dev/api/v2/entries/en/';
+    let postion = 0;
+    let allInputs = document.querySelectorAll("input");
+    console.log(newWord)
+    if(newWord === object.endWord) {
+        alert("You win");
+    }
+    allInputs.forEach((input, index)=>{
+        if(input.value === object.startWord[index])
+            postion = index
+        
+    })
+    
+    let diff = 0;
+    for (let i = 0; i < newWord.length; i++) {
+        if(newWord[i] !== object.startWord[i]) {
+            diff++;  
+        }
+    }
+    
+    if (diff === 1) {
+        let response = await fetch(`${BASE_URL}${newWord}`);
+        if(response.status === 404){
+            newWord = object.startWord
+            alert("This word is not an valid English word ")
+        }
+        else{
+            object.startWord = newWord
+        }
+        console.log(response);
+        
+    }
+    
+}
+
+
+
+function gettingNewWord(wordObject){
+    let allInputs = document.querySelectorAll('input');
+    document.addEventListener('keydown', (event) =>{
+        let inputValue = '';
+        if (event.key === "Enter"){
+            allInputs.forEach((input, index)=>{
+                inputValue += input.value.toUpperCase()
+                
+            })
+        gameEngin(wordObject, inputValue)
+            //console.log('This is the new output',inputValue)
+        }
+    });
+   
+}
+
+
+function createWordInput(word){
+    let wordObject = word;
+    let section = document.querySelector('.word-box');
+    let message = document.createElement('h2')
+    message.innerText = `${word.msg}`;
+    section.append(message);
+    for (let i = 0; i < word.startWord.length; i++) {
+        //console.log(i);
+        let inputtoHMTL = document.createElement('input');
+        inputtoHMTL.setAttribute('type', "text");
+        inputtoHMTL.setAttribute('maxlength', "1");  
+        inputtoHMTL.setAttribute('onClick', 'this.select()');
+        inputtoHMTL.setAttribute('value', word.startWord[i]);
+        section.append(inputtoHMTL);
+    }  
+    gettingNewWord(wordObject)
+}
+
+function randomWord(){
+    let wordObject = wordsWithMsg[Math.floor(Math.random()* wordsWithMsg.length)];
+    createWordInput(wordObject)
+}
+
+randomWord()
+
+
+
+
+function checkDifferentString(str1, str2) {
+    let diff = 0;
+    if (str1 === str2) return true;
+    let lengthDiff = Math.abs(str1.length - str2.length)
+    if (lengthDiff > 1) return false;
+  
+    for (let i=0; (i<str1.length || i < str2.length);i++) {
+      if (diff > 1) return false;
+      if (str1.charAt(i) !== str2.charAt(i)) diff++
+      start[index] !== newword[index]
+    }
+    if (diff <= 1) return true
+    else return false;
+  }
